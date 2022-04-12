7448c727e8df12cf28e084c3d2c5f28ee693f9f7
Alex M <bertkesha45@gmail.com>
2022-04-12 17:05:32 +0300

    searchSectionId fixed

--- src/components/TaskCreator.vue
+++ src/components/TaskCreator.vue
@@ -140,9 +140,10 @@ export default {
 
     searchSectionId() {
       let id = -1;
-      if (this.taskSectionName !== "None" || this.taskSectionName !=="" || this.taskSectionName !==null || this.taskSectionName !== undefined)
+      console.log(this.taskSectionName);
+      if (this.taskSectionName !== "None" && this.taskSectionName !=="" && this.taskSectionName !==null && this.taskSectionName !== undefined) {
         id = this.getSections.find((el) => el.name === this.taskSectionName).id;
-        console.log(id);
+        }
       return id;
     },
     searchLabelIdByName(labelName) {
